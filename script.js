1617afe093bbf58a8be6a11ffd6095b54afb7d2a
Ralds Oril <robertjswallow@gmail.com>
2020-12-09 13:00:27 -0800

    Added const timeBlockEl, added data-hour class to timeblocks

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 let dateEl = document.getElementById("currentDay");
+// Create variabe that = moment
 let timeVar = moment();
-let currentTime = timeVar.format("HH");
+console.log(moment());
+let currentTime = timeVar.format("LT");
 console.log(currentTime);
 dateEl.innerHTML = timeVar.format("dddd, MMMM Do");
+const timeBlockEls = document.querySelectorAll(".timeblock")
+// Create a for loop that loops through each time block. 
+for (let i = 0; i < timeBlockEls.length; i++) {
+console.log(timeBlockEls[i].id)
+   
+}
+
+
+  
+// // After we create for loops make if statement checking if the current hours is greater then the block hour. 
+
+//     if (currentTime < "#id") {
+        
+//     } else if (currentTime > "#id" ) {
+
+//     } else if (currentTime === "#id")
+
+// }
+
+// local storage
+
+$(".saveBtn").on("click", function(){
+    var title = $(this).parent().children("textarea").val();
+    var currentHour = $(this).parent().attr("data-hour");
+console.log(title);
+console.log(currentHour);
+// localStorage.setItem("appts" + currentHour, JSON.stringify(title));
+});
