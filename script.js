4ee90092fcb65f56a27330510147d9d6b9747caf
Ralds Oril <robertjswallow@gmail.com>
2020-12-11 13:17:10 -0800

    able to pull from local storage, timeblocks now change color

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
 let dateEl = document.getElementById("currentDay");
 // Create variabe that = moment
 let timeVar = moment();
-console.log(moment());
-let currentTime = timeVar.format("LT");
+console.log(timeVar);
+// let currentTime = parseInt(timeVar.format("k"));
+let currentTime = 12
 console.log(currentTime);
 dateEl.innerHTML = timeVar.format("dddd, MMMM Do");
 const timeBlockEls = document.querySelectorAll(".timeblock")
 // Create a for loop that loops through each time block. 
 for (let i = 0; i < timeBlockEls.length; i++) {
-console.log(timeBlockEls[i].id)
-   
-}
+    let currentEl = timeBlockEls[i];
+
+console.log("currentEl", currentEl.id);
+let currentHour = parseInt(currentEl.getAttribute("data-hour"));
+console.log("currentHour", currentHour);
+// After we create for loops make if statement checking if the current hours is greater then the block hour. 
+
+    if (currentTime < currentHour) {
+        $(currentEl).addClass('future');
+    } else if (currentTime > currentHour ) {
+        $(currentEl).addClass('past');
+    } else if (currentTime === currentHour) {
+        $(currentEl).addClass('present');
+    }
+
+    let val = localStorage.getItem(currentHour);
+    console.log(val);
+    
+    $(currentEl).children("textarea").val(val)
+};   
+
 
 
   
@@ -27,9 +46,12 @@ console.log(timeBlockEls[i].id)
 // local storage
 
 $(".saveBtn").on("click", function(){
-    var title = $(this).parent().children("textarea").val();
+    var val = $(this).parent().children("textarea").val();
     var currentHour = $(this).parent().attr("data-hour");
-console.log(title);
+console.log(val);
 console.log(currentHour);
-// localStorage.setItem("appts" + currentHour, JSON.stringify(title));
+localStorage.setItem(currentHour, val);
 });
+
+
+
